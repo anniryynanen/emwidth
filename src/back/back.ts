21b6cbfd9e1639ee2678bcbbfb32ad7f134e1829
Anni Ryynänen <anni@anniryynanen.com>
2024-11-11 17:40:52 +0200

    Setup Vue

--- src/back/back.ts
+++ src/back/back.ts
@@ -2,6 +2,6 @@ import {Hono} from "hono";
 import {serveStatic} from "hono/deno";
 
 const app = new Hono()
-    .use("/", serveStatic({path: "./index.html"}));
+    .use("*", serveStatic({root: "./"}));
 
 Deno.serve({hostname: "127.0.0.1", port: 8080}, app.fetch);
